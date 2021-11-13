bd32315cebf00bc5f98475e041008f17babfa160
Matthew Renze <merenze@gmail.com>
2021-11-12 22:01:46 -0600

    Add config

---
+++ example.local.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return (object) [
+    'database' => [
+        // 'servername' => '<database server here>',
+        // 'username' => '<database user here>',
+        // 'password' => '<password here>',
+    ]
+];
